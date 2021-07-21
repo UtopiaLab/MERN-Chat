b9de1d014b280c3b15cae0a7595ddae58b76024f
UtopiaLab <89520@fhss.sjp.ac.lk>
2021-07-21 18:34:04 +0530

    backend init

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT;
+const app = express();
+const server = http.createServer(app);
+
+app.use([
+    cors(),
+    express.static('uploads'),
+    bodyParser.json(),
+    bodyParser.urlencoded({extended: false}),
+])
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
