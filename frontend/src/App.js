bf9746768d16ea4fe1661963a492ce64b8f425a0
UtopiaLab <89520@fhss.sjp.ac.lk>
2021-07-23 15:42:12 +0530

    login input success

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,37 @@ import './styles/App.scss';
 import Login from './components/login';
 import ChatBody from './components/chatbody';
 import SideBar from './components/sidebar';
+import {BASE_URL, LOGIN} from './utils/apiEndpoints';
+import {postRequest} from './utils/apiRequests';
+import { useState } from 'react';
 
 function App() {
+
+  const [error, setError] = useState(null);
+  const [userObj, setUserObj] = useState(null);
+
+  const handleLogin = async (userData) => {
+    const formData = new FormData();
+    if(userData.file) {
+      formData.append("profileImg", userData.file, userData.file.name);
+    }
+    formData.append("payload", JSON.stringify({name: userData.name}));
+
+    const response = await postRequest(`${BASE_URL}${LOGIN}`, formData);
+    console.log(response);
+
+    if(response.error) {
+      setError(response.error);
+      return false;
+    }
+
+    setUserObj(response);
+  }
+
   return (
     <>
     {true ? (
-      <Login />
+      <Login handleLogin={handleLogin} />
       ) : (
       <div className="App">
         <div className="sidebar">
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,22 +2,42 @@ import '../styles/login.scss';
 import logo from '../images/logo.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-const Login = () => {
+const Login = ({handleLogin}) => {
+
+    const [user, setUser] = useState({
+        name: "",
+        file: ""
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleLogin(user)
+    }
+
+    const handleNameChange = (e) => {
+        setUser({...user, name: e.target.value});
+    }
+
+    const onFileChange = (e) => {
+        setUser({...user, file: e.target.files[0]});
+    }
+
     return (
         <div className="login-container">
             <div className="logo">
                 <img alt="logo" src={logo} />
             </div>
             <div className="login-form">
-                <form>
+                <form onSubmit={(e) => handleSubmit(e)}>
                     <div className="profile-image">
-                        <input className="file-upload" type="file" />
+                        <input className="file-upload" type="file" onChange={(e) => onFileChange(e)} />
                         <FontAwesomeIcon className="icon-block" icon={faUser} />
                     </div>
                     <div className="profile-name">
                         <FontAwesomeIcon className="icon-block" icon={faUser} />
-                        <input placeholder="Your Name Here!" type="text" name="name" />
+                        <input placeholder="Your Name Here!" type="text" name="name" onChange={(e) => handleNameChange(e)} />
                     </div>
                     <input type="submit" className="profile-submit" value="Join Now" />
                 </form>
