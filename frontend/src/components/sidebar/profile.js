b9348600c85e7f2edc8d71a0031db7d3cdfac098
UtopiaLab <89520@fhss.sjp.ac.lk>
2021-07-25 16:15:37 +0530

    code adjustments

--- frontend/src/components/sidebar/profile.js
+++ frontend/src/components/sidebar/profile.js
@@ -1,5 +1,5 @@
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSignOutAlt, faUser} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import '../../styles/profile.scss';
 import AuthContext from '../../contexts/authContext';
 import { useContext } from 'react';
@@ -10,7 +10,11 @@ const Profile = ({handleLogout}) => {
     return (
         <div className="profile">
             <div className="img-container">
-                <img alt="profile" src={profileImg} />
+                {profileImg ? (
+                    <img alt="profile" src={profileImg} />
+                ) : (
+                    <FontAwesomeIcon className="icon-block" icon={faUser} />
+                )}
             </div>
             {name}
             <div className="action-item" onClick={handleLogout}>
